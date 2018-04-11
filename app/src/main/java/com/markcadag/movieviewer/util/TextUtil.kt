8ab65eb4c26d7f15cb0a66f7ec3ef9dfb4cff235
mark cadag <markcadag@gmail.com>
2018-04-11 14:24:19 +0800

    Set default value of price to Php 0.00

--- app/src/main/java/com/markcadag/movieviewer/util/TextUtil.kt
+++ app/src/main/java/com/markcadag/movieviewer/util/TextUtil.kt
@@ -16,6 +16,12 @@ object TextUtil {
      * Format double value to readable price text
      */
     fun toFormattedPrice(price : Double) : String {
+        /**
+         * Set default value of price
+         */
+        if(price == 0.00) {
+            return "Php 0.00"
+        }
         var bd = BigDecimal(price)
         val f = DecimalFormat("###,###.00")
         bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP)
